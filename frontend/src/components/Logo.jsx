2a11db997f29b0188dafd0cc60b0137ee1916f53
vvaasuuu <vasumathim04@gmail.com>
2025-03-16 13:32:54 +0800

    updated logo.png path, resized logo and animated logo on login page

--- frontend/src/components/Logo.jsx
+++ frontend/src/components/Logo.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import "./Logo.css";
-import logo from "C:/Users/mani7/Desktop/NTU CEG/Y2S2/SC2006/Logo.png";
+import logo from "../assets/Logo.png";
 
 const Logo = () => {
     return(
         <div className="logo-container">
-            <img src={logo} alt="Logo" className={Logo} />
+            <img src={logo} alt="Logo" className="logo" />
         </div>
     );
 };
