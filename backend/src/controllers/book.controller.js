8d37c750c593439fe80fc7f2676e4286cb9a2a3f
Shapriyan19 <priyanmartian@gmail.com>
2025-03-19 21:00:13 +0800

    sort books

--- backend/src/controllers/book.controller.js
+++ backend/src/controllers/book.controller.js
@@ -13,6 +13,20 @@ export const searchBook= async (req,res)=>{
     
 };
 
+export const sortBook = async (req,res)=>{
+    try {
+        const {book,sort}=req.body;
+        const result=await fetch(`https://openlibrary.org/search.json?q=${book}&sort=${sort}`).then(a=>a.json());
+        if (!result){
+            return res.status(400).json({message:"Book not found"});
+        }
+        return res.status(200).json(result);
+    } catch (error) {
+        console.log("error in sortBook: ",error);
+        res.status(500).json({message:"Internal server error"});
+    }   
+}
+
 export const searchAuthor = async (req,res)=>{
     try{
         const{author}=req.body;
@@ -20,7 +34,7 @@ export const searchAuthor = async (req,res)=>{
         if (!result){
             return res.status(400).json({message:"Author not found"});
         }
-        res.status(200).json(result);
+        return res.status(200).json(result);
     }catch(error){
         console.log("error in searchByAuthor: ",error);
         res.status(500).json({message:"Internal server error"});
--- backend/src/routes/book.routes.js
+++ backend/src/routes/book.routes.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { searchBook, searchAuthor } from "../controllers/book.controller.js";
+import { searchBook, searchAuthor, sortBook } from "../controllers/book.controller.js";
 
 const router=express.Router();
 
 router.get("/search",searchBook);
 router.get("/search-author",searchAuthor);
+router.get("/sort-book",sortBook);
 
 export default router;
