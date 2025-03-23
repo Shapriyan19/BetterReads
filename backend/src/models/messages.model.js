8a27dab47082c5614a8135c08415fe24a54352d2
Shapriyan19 <priyanmartian@gmail.com>
2025-03-23 19:39:44 +0800

    socket io setup

--- backend/src/models/messages.model.js
+++ backend/src/models/messages.model.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const messageSchema=new mongoose.Schema({
     senderId: {type: mongoose.Schema.Types.ObjectId, ref: "User", required: true},
-    receiverId: {type: mongoose.Schema.Types.ObjectId, ref: "User", required: true},
+    club:{type: mongoose.Schema.Types.ObjectId, ref: "Club", required: true},
     text: {type: String},
     image: {type: String},
 },{timestamp: true});
